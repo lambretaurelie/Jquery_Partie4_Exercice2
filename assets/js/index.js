8139935b247a9bf7720065bb3f34b3f1e758d74e
lambret <aurelie.l-02@hotmail.fr>
2018-10-19 16:27:30 +0200

    exercice2 correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,35 @@
 $(function(){
-  $("#valid").click(function(){
-    //on déclare les variables avec la valeur des notes
+
+  $('#average').click(function(){
     math = $('#math').val();
     francais = $('#francais').val();
     geo = $('#geo').val();
     svt = $('#svt').val();
-    eps = $('#esp').val();
-    if(isNaN(math) == false && isNaN(francais) == false && isNaN(geo) == false && isNaN(svt) == false && isNaN(esp) == false){
-      //on fait la moyenne de ces notes
-      result = (parseFloat(math) + parseFloat(francais) + parseFloat(geo) + parseFloat(svt) + parseFloat(eps)) / 5;
-      if(result == 20){
-        message = "exellent";
-      }else if(result >= 0 && result < 10){
-        message = "En dessous de la moyenne!";
-      }else if(result >= 10 && result < 13){
-        message = "Moyen!";
-      }else if(result >= 13 && result < 16){
-        message = "Bien!";
+    eps = $('#eps').val();
+    console.log(math)
+    // je vérifie que chaque variable est bien un nombre compris entre 0 et 20
+    if ((isNaN(math) == false) && (isNaN(francais) == false) && (isNaN(geo) == false) && (isNaN(svt) == false) && (isNaN(eps) == false) &&
+    parseFloat(math) >= 0 && parseFloat(math) <= 20 &&
+    parseFloat(francais) >= 0 && parseFloat(francais) <= 20 &&
+    parseFloat(geo) >= 0 && parseFloat(geo) <= 20 &&
+    parseFloat(svt) >= 0 && parseFloat(svt) <= 20 &&
+    parseFloat(eps) >= 0 && parseFloat(eps) <= 20 ){
+      //calcule de ma moyenne
+      result = (parseFloat(math)+parseFloat(francais)+parseFloat(geo)+parseFloat(svt)+parseFloat(eps))/5;
+      if (result == 20){
+        message = "excellent";
       }else if(result >= 16 && result < 20){
-        message = "Tres Bien!";
+        message = "trés bien";
+      }else if(result >= 13 && result < 16){
+        message = "bien";
+      }else if(result >= 10 && result < 13){
+        message = "moyen";
+      }else if(result >= 0 && result < 10){
+        message = "en dessous de la moyenne";
       }
-      alert('Votre moyenne est de ' + result + 'elle est ' + message);
+      alert('la moyenne est : ' + result + ' l\'aprréciation est : ' + message);
     }else{
-      alert('Veuilliez entrer des nombres compris entre 0 et 20');
+      alert('merci de renseigner des nombres compris entre 0 et 20');
     }
   });
 });
